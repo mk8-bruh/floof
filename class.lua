a375577fea02e0b472a598bc3d45a88f6e5cf949
mk8 <153662482+mk8-bruh@users.noreply.github.com>
2025-04-09 23:59:43 +0200

    class module update

--- class.lua
+++ class.lua
@@ -1,71 +1,75 @@
 local _PATH = (...):match("(.-)[^%.]+$")
 local object = require(_PATH .. ".object")
 
-local class = {}
-local classes = setmetatable({}, {__mode = "k"})
-local named = setmetatable({}, {__mode = "v"})
-local objects = setmetatable({}, {__mode = "k"})
+local class  = {}
+local named  = setmetatable({}, {__mode = "v"}) -- { name : class }
+local clrefs = setmetatable({}, {__mode = "k"}) -- { class: self  | object: class }
+local supers = setmetatable({}, {__mode = "k"}) -- { class: super | object: super }
+
+function class.is(o, u)
+    return clrefs[o] == u
+end
+
+function class.class(o)
+    return clrefs[o]
+end
+
+function class.super(o)
+    return supers[o]
+end
 
 local function _create(c, ...)
-    if not c then return object.new(), 0 end
-    local o, d = _create(c.super, ...)
-    o.indexes:push(c, 1)
-    classes[c][o] = true
-    objects[o] = c
-    for k, v in pairs(c.blueprint) do
-        o[k] = v
+    if not c then
+        local o = object.new()
+        o.indexes:push(class)
+        return o, 0
     end
+    local o, d = _create(supers[c], ...)
+    o.indexes:push(c, 1)
+    clrefs[o] = c
+    supers[o] = supers[c]
     if type(o.init) == "function" then
         o:init(...)
     end
     return o, d + 1
 end
 
-function class.create(c, ...)
-    if not classes[c] then error(("Must specify a valid class object (received %s)"):format(type(c)), 2) end
+local function create(c, ...)
+    if not object[c] == c then error(("Must specify a valid class object (received %s)"):format(type(c)), 2) end
     local o, d = _create(c, ...)
     o.protectIndexes(-d, -1)
     return o
 end
 
-function class.new(...)
+local function new(_, ...)
     local arg = {...}
     if arg[1] and type(arg[1]) ~= "string" then
         table.insert(arg, 1, nil)
     end
-    if arg[2] and not classes[arg[2]] then
+    if arg[2] and object[arg[2]] ~= arg[2] then
         table.insert(arg, 2, nil)
     end
     if not type(arg[3]) == "table" then
         table.insert(arg, 3, {})
     end
-    local name, super, blueprint = arg[1], arg[2], arg[3]
-    local c = {}
-    classes[c] = setmetatable({}, {mode = "k"})
-    if name then named[name] = c end
-    local int = {
-        name = name,
-        super = super,
-        blueprint = blueprint
-    }
+    local name, super, c = arg[1], arg[2], arg[3]
+    if name then
+        if named[name] then
+            error(("A class named %q already exists"):format(name), 2)
+        elseif class[k] then
+            error(("Invalid class name: %q. Please choose a different name"):format(name), 2)
+        else
+            named[name] = c
+        end
+    end
+    name = name or tostring(c):match("table: (.+)") or tostring(c)
+    clrefs[c] = c
+    supers[c] = super
     return setmetatable(c, {
-        __index = function(c, k) return int[k] or class[k] end,
-        __newindex = function(c, k, v)
-            if not int[k] and not class[k] then
-                rawset(c, k, v)
-            end
-        end,
-        __call = class.create,
-        __tostring = function(c) return ("class: %s"):format(c.name or tostring(c):match("table: (.+)") or tostring(c)) end
+        __metatable = {},
+        __tostring = function(c) return ("class: %s"):format(name) end,
+        __call = create
     })
-end
-
-function class.is(c, o)
-    return classes[c] and classes[c][o] or false
-end
-
-function class.classOf(o)
-    return objects[o] or false
 end
 
 return setmetatable({}, {
@@ -73,5 +77,5 @@ return setmetatable({}, {
     __newindex = function() end,
 	__metatable = {},
     __tostring = function() return "FLUFFI class module :3" end,
-    __call = function(_, ...) return class.new(...) end,
+    __call = new,
 })
